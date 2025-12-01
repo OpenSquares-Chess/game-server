70b9747142303a834508854d44d22468b5e11f97
hrotovb001 <64749404+hrotovb001@users.noreply.github.com>
2025-12-01 15:26:52 -0800

    [FEATURE] Auth Token Validation & Room Token Logic + Validation (#3)
    * basic implementation of token verification
    
    * game server checks that token is meant for it
    
    * added new message types for token responses
    
    * completed real-time key impl + refactoring
    
    * bugfixes: container file + room key management

--- src/messages/received.rs
+++ src/messages/received.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize};
+use serde_with::{serde_as, DisplayFromStr};
 
+#[serde_as]
 #[derive(Deserialize)]
 pub struct ConnectionRequest {
     pub room: u32,
-    pub uuid: String
+    #[serde_as(as = "DisplayFromStr")]
+    pub key: u64
 }
